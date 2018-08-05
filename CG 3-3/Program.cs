b6f92c39b4fc29ac775817509e7ffc495a43a74d
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-08-05 07:20:24 -0500

    I need help with the code

--- CG 3-3/Program.cs
+++ CG 3-3/Program.cs
@@ -6,7 +6,18 @@ namespace CG_3_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //statements are an action that a program takes,ends in semi colon                
+            int x = 3;
+            int y = 4;
+            int z = x + y //is the expression
+            //operators are the symbols that specify the operation of the action + - * /
+            //operands are the players ie the integers x y and z are operands
+            Console.WriteLine("find the value of z");
+            Console.ReadLine();
+
+        }
+
+        
         }
     }
 }
